2fb25b81d5a35ae2e22ef11a76934018defd229b
XinZhang <zhangxin19@meituan.com>
2016-06-14 17:20:09 +0800

    V1.0.0 release.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # mobile-env
-Online chart to describe mobile compatibility issues.
+
+Online chart to collect mobile compatibility issues.
+
+# Features
+
+- Cooperate editing
+- Filter issues by device and browser
+
+
+# Tech Stack
+
+- Firebase
+- Webpack
+- React
+- ES6
+- Lodash
+- SVG
--- src/home/App.js
+++ src/home/App.js
@@ -12,9 +12,11 @@ export default class App extends Component {
     return (
       <article className="container">
         <section className="header">
-          <h2>
-            <a target="_blank" href="http://mp.weixin.qq.com/s?__biz=MzIzMDI3MTAzMA==&mid=2247483681&idx=1&sn=cffc82ffb3e0baf628a818152e141e01">关于移动端环境表</a>
-          </h2>
+          <h1>
+            兼容性缺陷收集工具
+          </h1>
+          <a target="_blank" href="http://mp.weixin.qq.com/s?__biz=MzIzMDI3MTAzMA==&mid=2247483681&idx=1&sn=cffc82ffb3e0baf628a818152e141e01">关于移动端环境表</a>
+          <a target="_blank" href="https://github.com/zhangxin840/mobile-env">Fork me on Github</a>
         </section>
         <Search />
         <Chart />
